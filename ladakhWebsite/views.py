0858e539c6c4c4b4b8a45facaa5b8601e46b7c32
Amruth Lal <amruth27m@gmail.com>
2018-09-27 20:49:47 +0530

    landing page WIP

--- ladakhWebsite/views.py
+++ ladakhWebsite/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from  django.http import HttpResponse
+from  django.shortcuts import render
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Hello world')
+
+
+
+def contactUs(request):
+    return HttpResponse('Contact us')
+
+
+def home(request):
+    return render(request,'ladakhWebsite/index.html')
+
+def testBulma(request):
+    return render(request,'ladakhWebsite/testBulma.html')
